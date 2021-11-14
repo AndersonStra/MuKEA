f0303ccee2a553f587096b2fc8ff0a3e322781f4
丁阳 <34742504+AndersonStra@users.noreply.github.com>
2021-11-14 11:16:57 +0800

    Update prepare_annotation.py

--- okvqa/prepare_annotation.py
+++ okvqa/prepare_annotation.py
@@ -3,11 +3,11 @@ import pickle
 
 row = {}
 
-with open('/data2/yjgroup/lmz/pr*/data/okvqa6_1/raw_data/train_36bbox_res_okvqa2.pickle', 'rb') as f:
+with open('/pr*/data/okvqa6_1/raw_data/train_36bbox_res_okvqa2.pickle', 'rb') as f:
     image_raw = pickle.load(f, encoding='iso-8859-1')
-with open('/data2/yjgroup/dy/kb-vqa/data/ok-vqa/OpenEnded_mscoco_train2014_questions.json') as f:
+with open('/kb-vqa/data/ok-vqa/OpenEnded_mscoco_train2014_questions.json') as f:
     ques = json.load(f)
-with open('/data2/yjgroup/dy/kb-vqa/data/ok-vqa/mscoco_train2014_annotations.json') as f:
+with open('/kb-vqa/data/ok-vqa/mscoco_train2014_annotations.json') as f:
     annotation = json.load(f)
 
 ques = ques['questions']
@@ -23,5 +23,5 @@ for q, a in zip(ques, annotation):
         multi_answers.append(ans['raw_answer'])
     row[q['question_id']] = {'question':question, 'image_id':image_id, 'multi_answers':multi_answers,'label':origin_labels}
 
-with open('/data2/yjgroup/dy/kb-vqa/data/okvqa_train_raw_answer.json','w') as f:
+with open('/kb-vqa/data/okvqa_train_raw_answer.json','w') as f:
     json.dump(row, f, indent=4)
