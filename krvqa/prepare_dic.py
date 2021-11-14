39451d64fc3c2187382be5692da2d559f25b6bbb
丁阳 <34742504+AndersonStra@users.noreply.github.com>
2021-11-14 11:14:27 +0800

    remove private

--- krvqa/prepare_dic.py
+++ krvqa/prepare_dic.py
@@ -3,7 +3,7 @@ import pickle
 import collections
 
 word_counts = collections.Counter()
-with open('/data2/yjgroup/dy/kb-vqa/data/kr-vqa/krvqa_train.json','r') as f:
+with open('/kb-vqa/data/kr-vqa/krvqa_train.json','r') as f:
     train_row = json.load(f)
 
 for qid, item in train_row.items():
@@ -13,5 +13,5 @@ for qid, item in train_row.items():
 vocabulary_inv = [x[0] for x in word_counts.most_common()]
 vocabulary = {x: i for i, x in enumerate(vocabulary_inv)}
 
-with open('/data2/yjgroup/dy/kb-vqa/data/kr-vqa/krvqa-ans_dic.pickle', 'wb') as f:
+with open('/kb-vqa/data/kr-vqa/krvqa-ans_dic.pickle', 'wb') as f:
     pickle.dump(vocabulary, f)
