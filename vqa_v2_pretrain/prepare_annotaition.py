58fa7426608f425eecc51a05b95bc600439f98ab
丁阳 <34742504+AndersonStra@users.noreply.github.com>
2021-11-23 15:26:30 +0800

    Update prepare_annotaition.py

--- vqa_v2_pretrain/prepare_annotaition.py
+++ vqa_v2_pretrain/prepare_annotaition.py
@@ -13,12 +13,12 @@ for q_a in zip(ques_list, ques_list):
     q_id = str(q_a[0]['question_id']) + 'f'
     question = q_a[0]['question']
     image_id = q_a[0]['image_id']
-    # ans = q_a[1]['multiple_choice_answer']
-    # multi_ans = q_a[1]['answers']
-    ans = ['unknown']
-    multi_list = ans * 10
-    # for i in multi_ans:
-    #     multi_list.append(i['answer'])
+    ans = q_a[1]['multiple_choice_answer']
+    multi_ans = q_a[1]['answers']
+    #ans = ['unknown']
+    #multi_list = ans * 10
+    for i in multi_ans:
+        multi_list.append(i['answer'])
     item = {'question': question, 'image_id': image_id, 'answer': ans,'multi_answers': multi_list}
     dic[q_id] = item
 with open('/kb-vqa/data/vqa_test.json','w') as f:
