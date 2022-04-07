d5da4ee9bed5fd43e643eeda5e0d0040542ad5f9
丁阳 <34742504+AndersonStra@users.noreply.github.com>
2022-04-07 19:27:45 +0800

    fix trainval

--- vqa_v2_pretrain/tsv2feature.py
+++ vqa_v2_pretrain/tsv2feature.py
@@ -3,7 +3,7 @@
 
 import base64
 import pickle
-
+import json
 import numpy as np
 import csv
 import sys
@@ -15,15 +15,22 @@ from tqdm import tqdm
 csv.field_size_limit(sys.maxsize)
 
 FIELDNAMES = ['image_id', 'image_w', 'image_h', 'num_boxes', 'boxes', 'features']
+train_img_list = []
+
+with open('../data/vqa_train.json', 'r') as f:
+    vqa_raw = json.load(f)
+for raw in vqa_raw.values():
+    train_img_list.append(str(raw['image_id']))
 
 
 def tsv2feature(split):
-    if split == 'train':
-        infile = '../data/train2014_36/train2014_resnet101_faster_rcnn_genome_36.tsv'
-    elif split == 'val':
-        infile = '../data/val2014_36/val2014_resnet101_faster_rcnn_genome_36.tsv'
+    if split == 'trainval':
+        infile = '../data/trainval2014_36/trainval2014_resnet101_faster_rcnn_genome_36.tsv'
+    elif split == 'test':
+        infile = '../data/test2015_36/test2015_resnet101_faster_rcnn_genome_36.tsv'
     # Verify we can read a tsv
-    in_data = {}
+    in_data_train = {}
+    in_data_val = {}
     with open(infile, "r") as tsv_in_file:
         reader = csv.DictReader(tsv_in_file, delimiter='\t', fieldnames=FIELDNAMES)
         for item in tqdm(reader):
@@ -37,10 +44,20 @@ def tsv2feature(split):
                                             dtype=np.float32).reshape((item['num_boxes'], -1))
             spatial_feature = np.concatenate((item['boxes'][:, :2] / w_h,
                                               item['boxes'][:, 2:] / w_h), axis=1)
-            in_data[item['image_id']] = {'feats': item['features'], 'sp_feats': spatial_feature}
-    with open('../data/vqa_img_feature_%s.pickle' % split, 'wb') as f:
-        pickle.dump(in_data, f)
+            if item['image_id'] in train_img_list:
+                in_data_train[item['image_id']] = {'feats': item['features'], 'sp_feats': spatial_feature}
+            else:
+                in_data_val[item['image_id']] = {'feats': item['features'], 'sp_feats': spatial_feature}
+    print(len(in_data_train), len(in_data_val))
+    if split == 'trainval':
+        with open('../data/vqa_img_feature_train.pickle', 'wb') as f:
+            pickle.dump(in_data_train, f)
+        with open('../data/vqa_img_feature_val.pickle', 'wb') as f:
+            pickle.dump(in_data_val, f)
+    else:
+        with open('../data/vqa_img_feature_%s.pickle' % split, 'wb') as f:
+            pickle.dump(in_data_val, f)
 
 if __name__ == "__main__":
-    tsv2feature('train')
-    tsv2feature('val')
+    tsv2feature('trainval')
+    # tsv2feature('val')
